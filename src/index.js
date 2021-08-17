d306e9545360d0b689bf17072b055783b8cf4bad
MikolaSh <nikolashulga2000@gmail.com>
2021-08-17 16:00:45 +0300

    feat: solution for typical array problem task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array===undefined||array.length==0){return 0;}
+  var min = array[0];
+  for(let arr of array){
+    if(arr<min){min = arr}
+  }
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array===undefined||array.length==0){return 0;}
+  var max = array[0];
+  for(let arr of array){
+    if(arr>max){max = arr}
+  }
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array===undefined||array.length==0){return 0;}
+  var avr=0;
+  for(var i=0;i<array.length;i++){
+    avr+=array[i]
+  }
+  avr = avr/array.length;
+  return avr;
 }
